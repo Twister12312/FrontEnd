fad4b4b68f4e8669a7c464d2f187b47b875de940
narm2cool <narmadhamayilraj@gmail.com>
2022-10-21 20:14:48 +0530

    login and register FE

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,34 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css'],
+})
+export class AdminComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  signupForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      username: new FormControl(null, Validators.required),
+      password: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+    this.router.navigate(['/myaadhar2']);
+  }
+
+  get() {
+    throw new Error('Method not implemented.');
+  }
+  gotoRegister() {
+    this.router.navigate(['/myaadhar2']);
+  }
+}
